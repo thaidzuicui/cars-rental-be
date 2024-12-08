ba89956bb686a511aae651d4e08efa016c4afab4
HNam22 <hoainam.22tq@gmail.com>
2024-12-08 10:38:04 +0700

    config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 // Cấu hình kết nối
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tables'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT,
 });
 
 // Kết nối database

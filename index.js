a81bddccf136ca6a9c4982b5a2452cdd7a079032
thaidzuicui <hthai313204@gmail.com>
2024-12-20 14:13:51 +0700

    deploy

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ const bodyParser = require("body-parser");
 const db = require("./config/db");
 const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const path = require("path");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORTT || 3001;
 // Middleware
 app.use(bodyParser.json());
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "dist")));
 
 // Routes
 app.use("/api/cars", require("./routes/cars"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/rentals", require("./routes/rentals"));
 app.use("/api/reviews", require("./routes/reviews"));
-app.use("/api/auth", require("./routes/auth")); 
-app.use("/api/likes", require("./routes/likes")); 
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/likes", require("./routes/likes"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 // Start server
 app.listen(PORT, () => {
